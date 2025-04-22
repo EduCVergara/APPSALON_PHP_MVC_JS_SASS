aa5c6f0f6ec669f4f5cdf53829b734f910846b4a
Edu Constancio Vergara <e.constanciovergara@gmail.com>
2025-04-22 17:15:21 -0400

    Paginador y navegación de servicios

--- src/js/app.js
+++ src/js/app.js
@@ -17,4 +17,86 @@ function initPasswordToggles() {
   
 document.addEventListener('DOMContentLoaded', () => {
     initPasswordToggles();
+    iniciarApp();
 });
+
+let paso = 1;
+const pasoInicial = 1;
+const pasoFinal = 3;
+
+function iniciarApp() {
+  mostrarSeccion(); // Muestra y oculta las secciones
+  tabs(); // Cambia la sección cuando se presionan los tabs
+  botonesPaginador(); // Agrega o quita los botones del paginador
+  paginaSiguiente();
+  paginaAnterior();
+}
+
+function mostrarSeccion() {
+  
+  //Ocultar la sección que tenga la clase de mostrar
+  const seccionAnterior = document.querySelector('.mostrar');
+  const tabAnterior = document.querySelector('.actual');
+  if (seccionAnterior) {
+    seccionAnterior.classList.remove('mostrar');
+    tabAnterior.classList.remove('actual');
+  }
+
+  //Seleccionar la sección con el paso
+  const pasoSelector = `#paso-${paso}`;
+  const seccion = document.querySelector(pasoSelector);
+  seccion.classList.add('mostrar');
+
+  // Resalta el tab actual
+  const tab = document.querySelector(`[data-paso="${paso}"]`);
+  tab.classList.add('actual');
+}
+
+function tabs() {
+  const botones = document.querySelectorAll('.tabs button');
+
+  botones.forEach(boton => {
+    boton.addEventListener('click', function(e) {
+      paso = parseInt(e.target.dataset.paso);
+      mostrarSeccion();
+      botonesPaginador();
+    })
+  })
+}
+
+function botonesPaginador() {
+
+  const paginaAnterior = document.querySelector('#anterior');
+  const paginaSiguiente = document.querySelector('#siguiente');
+
+  if (paso === 1) {
+    paginaAnterior.classList.add('ocultar');
+    paginaSiguiente.classList.remove('ocultar');
+  } else if (paso === 3) {
+    paginaAnterior.classList.remove('ocultar');
+    paginaSiguiente.classList.add('ocultar');
+  } else {
+    paginaAnterior.classList.remove('ocultar');
+    paginaSiguiente.classList.remove('ocultar');
+  }
+
+  mostrarSeccion();
+}
+
+function paginaAnterior() {
+  const paginaAnterior = document.querySelector('#anterior');
+  paginaAnterior.addEventListener('click', function() {
+    if (paso <= pasoInicial) return;
+    paso--;
+    botonesPaginador();
+  })
+}
+
+function paginaSiguiente() {
+  const paginaSiguiente = document.querySelector('#siguiente');
+  paginaSiguiente.addEventListener('click', function() {
+    if (paso >= pasoFinal) return;
+    paso++;
+    botonesPaginador();
+  })
+}
